4d5ff8c2c744c5fed7fa77ea3a6164fa98fce6fe
mehran-kh-z <khazaeizadeh.m@gmail.com>
2025-02-08 19:34:56 +0330

    minor changes

--- ports/command-system/portfile.cmake
+++ ports/command-system/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+        -DCMAKE_PREFIX_PATH="${CURRENT_INSTALLED_DIR};${QTDIR}/bin"
 )
 vcpkg_cmake_install()
 vcpkg_install_copyright("${SOURCE_PATH}/COPYING")
