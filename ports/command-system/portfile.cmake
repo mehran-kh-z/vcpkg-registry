2e2d548fb47a9924268378d780d79bade1a840c8
mehran-kh-z <khazaeizadeh.m@gmail.com>
2025-02-09 16:44:33 +0330

    minor changes

--- ports/command-system/portfile.cmake
+++ ports/command-system/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(    
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/mehran-kh-z/command-system
-    REF 57907b5738e96b599ce6207716cad1c02c00fc09
+    REF 79d18d8c1fc3b53dda6bceab5ca0213ff1a37658
     SHA256 0
     HEAD_REF main
 )
@@ -13,4 +13,4 @@ vcpkg_cmake_configure(
         -DCMAKE_PREFIX_PATH="${CURRENT_INSTALLED_DIR};${QTDIR}/bin"
 )
 vcpkg_cmake_install()
-vcpkg_install_copyright("${SOURCE_PATH}/COPYING")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
