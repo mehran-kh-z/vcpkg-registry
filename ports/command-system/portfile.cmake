181d17f6ab3acb013a5d5af8a22eb8f22c119bf3
mehran-kh-z <khazaeizadeh.m@gmail.com>
2025-02-09 14:23:32 +0330

    minor changes

--- ports/command-system/portfile.cmake
+++ ports/command-system/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_git(    
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/mehran-kh-z/command-system
-    REF c215ee4060f6c32510989243ec5f60e9701f75bc
-    SHA256 36c0e935e8222545952ca5841c82595b57cc2562b6729b38c355bd46acf7251b007cf1bcc8f5d839240b29ceac5dd90810f6f9aea945bcb85a073a20fd0840c1
+    REF b7a19d2bdcb26cb05c34bfbffb36bd237482dc00
+    SHA256 0
     HEAD_REF main
 )
 
